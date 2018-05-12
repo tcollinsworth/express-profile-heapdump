e94352a7fd0b32d935a5ba170e8c5e39fe152ae9
troy <troy@daisyworks.com>
2018-05-11 21:44:30 -0400

    reverted

--- express-profile-heapdump.js
+++ express-profile-heapdump.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const heapdump = require('heapdump')
-const profile = require('@risingstack/v8-profiler')
+const profile = require('v8-profiler')
 
 const fs = require('fs')
 const os = require('os')
